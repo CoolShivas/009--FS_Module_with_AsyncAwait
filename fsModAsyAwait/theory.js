15013ef62bb01ff6bf969c60b87e366c72ab77ed
CoolShivas <shivas2710cool00@gmail.com>
2025-03-11 15:27:50 +0530

    Starting of Node.js FS Module with Promises Async/Await

---
+++ fsModAsyAwait/theory.js
@@ -0,0 +1,21 @@
+/**
+ * // // Theory :-
+ * 
+ * 
+ * 
+ *  The fs/promises module provides a promise-based API for interacting with 
+    the filesystem, allowing asynchronous operations to be handled using 
+    modern JavaScript features like async/await or .then() chains. It is part of 
+    the fs module in Node.js but designed for those who prefer promises over 
+    callback-based or synchronous methods.
+
+ * 
+ * 
+ *  Purpose:-
+        Simplifies asynchronous file operations by using promises, 
+        making the code more readable and modern compared to traditional 
+        callback-based approaches.
+        
+ * 
+ * 
+ */
