16c5b5c9b88768124cef0a5893078473a9541847
CoolShivas <shivas2710cool00@gmail.com>
2025-03-11 16:23:16 +0530

    Resolve unnecessary occurrence of Undefined on Terminal as Output.

--- fsModAsyAwait/asyncAwait.js
+++ fsModAsyAwait/asyncAwait.js
@@ -32,21 +32,65 @@ const filePath = path.join(__dirname, fileName);
 
 
 
-const createFileFunc = async () => {
-    try {
-      const res = await fsPro.writeFile(
-        filePath,
-        "Formation of file using Promises and Async/Await.",
-        "utf-8"
-      );
-      console.log(res, "File Created Successfully!");
-    } catch (err) {
-      console.log("Error Creating File", err);
-    }
-  };
-  createFileFunc();
+// const createFileFunc = async () => {
+//     try {
+//       const res = await fsPro.writeFile(
+//         filePath,
+//         "Formation of file using Promises and Async/Await.",
+//         "utf-8"
+//       );
+//       console.log(res, "File Created Successfully!");
+//     } catch (err) {
+//       console.log("Error Creating File", err);
+//     }
+//   };
+//   createFileFunc();
   
 
 // // // // Here, we are getting the Output on Terminal as :-
 // // // // undefined File Created Successfully!
 // // // // Therefore, new file created named as "fsProAsyAwait.txt".
+
+
+///////--------------------------------------------------------------------------------------------
+
+
+
+// // // // Getting rid of unnecessary undefined Output on Terminal i.e., (undefined File Created Successfully!):-
+
+
+
+const createFileFunc = async () => {
+    try {
+      await fsPro.writeFile(
+        filePath,
+        "Formation of file using Promises and Async/Await.",
+        "utf-8"
+      );
+      console.log("File Created Successfully!");
+    } catch (err) {
+      console.log("Error Creating File", err);
+    }
+  };
+  createFileFunc();
+
+
+// // // // Here, we are getting the Output on Terminal as :-
+// // // // File Created Successfully!
+
+/**
+ *
+ * fsPromises.writeFile() :- Writes data to a file asynchronously. If the file exists, it overwrites the content.
+ *
+ * // ! syntax :- fsPromises.writeFile(path, data, options).
+ * // ? path :- The file path to write to.
+ * // ? data :- The content to write to the file.
+ * // ? options : Optional. Specifies encoding (e.g., 'utf8'), mode, or flags.
+ *
+ */
+
+
+
+////////************************************************************************************* */
+/////-------------------------------------------------------------------------------------------
+////////************************************************************************************* */
